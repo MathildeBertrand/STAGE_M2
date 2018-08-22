b5bc009749a7f11b39dfd52d67c66abc7210236a
MathildeBertrand <b.mathibert@gmail.com>
2018-08-22 22:43:18 +0200

    README

--- scripts/regions_uniques/AnnotGenes/outputAnnotate.sh
+++ scripts/regions_uniques/AnnotGenes/outputAnnotate.sh
@@ -10,5 +10,6 @@ grep -E "EID|DIE|OA|OB" outfile.txt > geneOfInterest.txt #On recupere les lignes
 #outfile.txt est le fichier de sortie du script annotate_domains_v3.0.py
 
 awk '{print $6}' geneOfInterest.txt > ListeGene.txt #On extrait le nom des genes marques DIE ou EID
+awk '{print $1}' picsOfInterest.txt > Listepics.txt #On extrait le nom des pics qui sont annotes sur des genes
 
-#La liste de genes est ensuite copiee dans Venny pour comparer les genes identifies dans les differentes conditions
+#La liste de genes est des pics sont ensuite copiees dans Venny pour comparer les genes identifies dans les differentes conditions (ou le nb de pics dans chaque cond)
